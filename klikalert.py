6a651919b407205e7b80ba873aa40c1ddd2c0a27
Lasbi Noveni <63292554+lasbinoveni@users.noreply.github.com>
2024-03-04 18:32:01 +0700

    melakukan klik pada tombol alert.

---
+++ klikalert.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+import time
+
+driver = webdriver.Chrome()
+
+driver.maximize_window()
+driver.get("https://demoqa.com/alerts")
+driver.find_element(By.ID,"alertButton").click()
+time.sleep(10)
+
+Alert = Alert(driver)
+Alert.accept()
